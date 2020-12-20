ea11c841e4be0f46d376379f707e00836a734d39
kai <kaikitagawajones@gmail.com.com>
2020-12-20 18:33:59 +0900

    Finished basic documentation.

--- fbg.h
+++ fbg.h
@@ -1,5 +1,5 @@
 //============================================================================//
-// framebuffer graphics
+// FrameBufferGraphics
 //============================================================================//
 
 //----------------------------------------------------------------------------//
@@ -24,24 +24,92 @@ typedef struct {
 
 } fbg_Screen;
 
-/*---- fbg_set_tty_graphics() ------------------------------------------------//
-	DESCRIPTION
-		Set tty to graphics mode. This stops the terminal overwriting the framebuffer.
-
-	ARGUMENTS
-		(NONE)
-
-	RETURN VALUE
-		0:	sucess
-		-1:	failure
-
-*/
+/*
+ * Set tty to graphics mode. This stops the terminal overwriting the
+ * framebuffer.
+ * 
+ * ARGUMENTS:
+ *   (NONE)
+ * 
+ * RETURN VALUE:
+ *   0:
+ *     Success.
+ *   -1:
+ *     Could not open fb file.
+ *   -2:
+ *     ioctl() failed.
+ *   -3:
+ *     mmap() failed.
+ */
 
 int fbg_set_tty_graphics();
+
+/* 
+ * Set tty to text mode. If fbg_set_tty_graphics() has been used, this function
+ * has to be called to be able to use the terminal again.
+ * 
+ * ARGUMENTS:
+ *   (NONE)
+ * 
+ * RETURN VALUE:
+ *   0:
+ *     Success.
+ *   -1:
+ *     Could not open tty file.
+ *   -2:
+ *     ioctl() failed.
+ */
+
 int fbg_set_tty_text();
 
+/*
+ * Initlialises fbg_Screen.
+ * 
+ * ARGUMENTS:
+ *   *screen:
+ *     fbg_Screen to initialise.
+ * 
+ * RETURN VALUE:
+ *   0:
+ *     Success.
+ *   -1:
+ *     Could not open tty file.
+ *   -2:
+ *     ioctl() failed.
+ */
+
 int fbg_init_screen(fbg_Screen *screen);
+
+/*
+ * Unmaps buffer in fbg_Screen.
+ * 
+ * ARGUMENTS:
+ *   *screen:
+ *     fbg_Screen to free.
+ * 
+ * RETURN VALUE:
+ *   (NONE)
+ */
+
 void fbg_free_screen(fbg_Screen *screen);
+
+/*
+ * Draws pixel to screen.
+ * 
+ * ARGUMENTS:
+ *   screen:
+ *     Screen to draw to.
+ *   x, y:
+ *     Position of pixel.
+ *   r, g, b:
+ *     Color of pixel.
+ * 
+ * RETURN VALUE:
+ *   0:
+ *     Success.
+ *   -1:
+ *     Pixel was out of bounds.
+ */
 
 int fbg_draw_pixel(fbg_Screen screen, int x, int y, int r, int g, int b);
 
@@ -68,7 +136,7 @@ int fbg_set_tty_graphics() {
 		return -1;
 
 	if(ioctl(tty_file, KDSETMODE, KD_GRAPHICS) < 0)
-		return -1;
+		return -2;
 
 	close(tty_file);
 	return 0;
@@ -95,10 +163,10 @@ int fbg_init_screen(fbg_Screen *screen) {
 	struct fb_fix_screeninfo fixed_info;
 
 	if(ioctl(fb_file, FBIOGET_FSCREENINFO, &fixed_info) < 0)
-		return -1;
+		return -2;
 
 	if(ioctl(fb_file, FBIOGET_VSCREENINFO, &variable_info) < 0)
-		return -1;
+		return -2;
 
 	screen->size = fixed_info.line_length * variable_info.yres;
 	screen->width = variable_info.xres;
@@ -111,7 +179,7 @@ int fbg_init_screen(fbg_Screen *screen) {
 
 	screen->buffer = (char*)mmap(0, screen->size, PROT_READ | PROT_WRITE, MAP_SHARED, fb_file, 0);
 	if(screen->buffer < 0)
-		return -1;
+		return -3;
 
 	close(fb_file);
 	return 0;
@@ -122,23 +190,8 @@ void fbg_free_screen(fbg_Screen *screen) {
 }
 
 int fbg_draw_pixel(fbg_Screen screen, int x, int y, int r, int g, int b) {
-	int returnValue = 0;
-
-	if(x < 0) {
-		x = 0;
-		returnValue = -1;
-	} else if(x > screen.width) {
-		x = screen.width;
-		returnValue = -1;
-	}
-
-	if(y < 0) {
-		y = 0;
-		returnValue = -1;
-	} else if(y > screen.height) {
-		y = screen.height;
-		returnValue = -1;
-	}
+	if(x < 0 || x > screen.width || y < 0 || y > screen.height)
+		return -1;
 
 	int offset = x * screen.pixelBytes + y * screen.lineBytes;
 				
@@ -146,7 +199,7 @@ int fbg_draw_pixel(fbg_Screen screen, int x, int y, int r, int g, int b) {
 	screen.buffer[offset + screen.offsetGreen] = g;
 	screen.buffer[offset + screen.offsetBlue] = b;
 
-	return returnValue;
+	return 0;
 };
 
 #endif
